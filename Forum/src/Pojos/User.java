94a522f987f7d0cc11da0fb1eb47e06dbcd203fc
zhantaojava <suzhantao@127.0.53.53>
2015-06-16 15:41:37 -0400

    User Create

--- Forum/src/Pojos/User.java
+++ Forum/src/Pojos/User.java
@@ -1,5 +1,54 @@
 package Pojos;
 
 public class User {
-
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	private int age;
+	private String name;
+	private String pw;
+	private String email;
+	private String sex;
+	private int grade;
+	
 }
